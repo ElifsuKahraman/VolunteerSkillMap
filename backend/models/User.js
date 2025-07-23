1dd748fe218d451bc42eadcdf6ccb998f5cf33d3
ElifsuKahraman <136170828+ElifsuKahraman@users.noreply.github.com>
2025-07-23 12:53:51 +0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },       
-  email: { type: String, required: true, unique:true},      
+  email: { type: String, required: true, unique:true},     
   password: { type: String, required: true },
   activities: [{ type: mongoose.Schema.Types.ObjectId, ref: "Activity" }],
 });
